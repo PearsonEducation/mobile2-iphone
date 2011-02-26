11b1aeae9402e86d43ed32b2ff3b7474d18165a0
Tony Hillerson <tony.hillerson@gmail.com>
2011-02-25 22:46:21 -0700

    Login View is logging in correctly. It also keeps the fields in place if the keyboard shows or hides. Animations may need work.

--- eCollege/eCollege/LogInViewController.h
+++ eCollege/eCollege/LogInViewController.h
@@ -9,11 +9,16 @@
 #import <UIKit/UIKit.h>
 #import "ECSession.h"
 
-@interface LogInViewController : UIViewController<UITextFieldDelegate, ECSessionAuthenticationDelegate> {
-    IBOutlet UITextField *clientStringText;
+@interface LogInViewController : UIViewController<UITextFieldDelegate> {
 	IBOutlet UITextField *usernameText;
 	IBOutlet UITextField *passwordText;
+	IBOutlet UISwitch *keepLoggedInSwitch;
 	IBOutlet UIButton *logInButton;
+	IBOutlet UIScrollView *scrollView;
+	
+	BOOL keyboardIsShowing;
+	CGPoint scrollViewOffsetWhenKeyboardIsHidden;
+	CGSize scrollViewSizeWhenKeyboardIsHidden;
 }
 
 - (IBAction) logInClicked:(id)caller;
--- eCollege/eCollege/LogInViewController.m
+++ eCollege/eCollege/LogInViewController.m
@@ -23,42 +23,95 @@
 #pragma mark - View lifecycle
 
 - (void)viewDidUnload {
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
     [super viewDidUnload];
+}
+
+- (void) viewDidLoad {
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector (keyboardDidShow:)
+												 name: UIKeyboardDidShowNotification object:nil];
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self 
+											 selector:@selector (keyboardDidHide:)
+												 name: UIKeyboardDidHideNotification object:nil];
+	scrollView.contentSize = self.view.frame.size;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-#pragma mark - Control callbacks
+#pragma mark - Control callbacks and view logic
+
+- (void) keyboardDidShow:(NSNotification *)notification {
+	if (keyboardIsShowing) {
+		return; //apparently we can sometimes get too many notifications
+	}
+	
+	NSDictionary *info = [notification userInfo];
+	NSValue *keyboardBoundsEndValue = [info objectForKey:UIKeyboardFrameEndUserInfoKey];
+	CGSize keyboardSize = [keyboardBoundsEndValue CGRectValue].size;
+	scrollViewOffsetWhenKeyboardIsHidden = scrollView.contentOffset;
+	scrollViewSizeWhenKeyboardIsHidden = scrollView.frame.size;
+	
+	[UIView beginAnimations:nil context:NULL];
+		[UIView setAnimationDuration:0.45];
+		[UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+		CGRect viewFrame = scrollView.frame;
+		viewFrame.size.height -= keyboardSize.height;
+		scrollView.frame = viewFrame;
+		
+		//TODO: some magic numbers here... clean them up
+		scrollView.contentOffset = CGPointMake(0, usernameText.frame.origin.y - 40);
+	[UIView commitAnimations];
+
+	keyboardIsShowing = YES;
+}
+
+- (void) keyboardDidHide:(NSNotification *)notification {
+	if (!keyboardIsShowing) {
+		return; //apparently we can sometimes get too many notifications
+	}
+
+	[UIView beginAnimations:nil context:NULL];
+		[UIView setAnimationDuration:0.45];
+		[UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+		scrollView.frame = CGRectMake(0, 0, scrollViewSizeWhenKeyboardIsHidden.width, scrollViewSizeWhenKeyboardIsHidden.height);
+		scrollView.contentOffset = scrollViewOffsetWhenKeyboardIsHidden;
+	[UIView commitAnimations];
+
+	keyboardIsShowing = NO;
+}
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-	if (textField == clientStringText) {
-		[usernameText becomeFirstResponder];
-	} else if (textField == usernameText) {
+	if (textField == usernameText) {
 		[passwordText becomeFirstResponder];
 	} else if (textField == passwordText) {
-		[self logInClicked:passwordText];
+		[passwordText resignFirstResponder];
 	}
 	return YES;
 }
 
 - (IBAction) logInClicked:(id)caller {
-	ECSession *session = [ECSession sharedSession];
-	session.authenticationDelegate = self;
 	NSString *clientId = [[ECClientConfiguration currentConfiguration] clientId];
 	NSString *clientString = [[ECClientConfiguration currentConfiguration] clientString];
 	NSString *username = usernameText.text;
 	NSString *password = passwordText.text;
+	BOOL keepLoggedIn = keepLoggedInSwitch.on;
+	ECSession *session = [ECSession sharedSession];
 	[session authenticateWithClientId:clientId
 						 clientString:clientString
 							 username:username
-							 password:password];
+							 password:password
+					 keepUserLoggedIn:keepLoggedIn
+							 delegate:self
+							 callback:@selector(sessionDidAuthenticate)];
 }
 
 #pragma mark - Authentication Complete
 
-- (void) sessionDidAuthenticate:(ECSession *)aSession {
+- (void) sessionDidAuthenticate {
 }
 
 @end
