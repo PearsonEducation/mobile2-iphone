a623c7dd7b8003d2b3ad971e2887b27f5dd9481e
Brad Umbaugh <brad.umbaugh@effectiveui.com>
2011-03-18 19:37:11 -0600

    Changed dropbox view controllers to show buttons for all files

--- eCollege/eCollege/DropboxMessageDetailViewController.m
+++ eCollege/eCollege/DropboxMessageDetailViewController.m
@@ -14,6 +14,7 @@
 #import <QuartzCore/CoreAnimation.h>
 #import "NSDateUtilities.h"
 #import "DateCalculator.h"
+#import "DropboxAttachment.h"
 
 
 @interface DropboxMessageDetailViewController ()
@@ -112,161 +113,171 @@
 
 - (void)handleErrors {
     NSLog(@"Errors loading dropbox basket / dropbox message");
+    UIFont* errorFont = [UIFont fontWithName:@"Helvetica-Bold" size:19];
+    NSString* error = NSLocalizedString(@"Error loading dropbox item",@"Statement that there is an error loading a dropbox item.");
+    CGSize maximumSize = CGSizeMake(320, 1000);
+    CGSize errorSize = [error sizeWithFont:errorFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap];
+    UILabel* errorLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2 - errorSize.width/2, self.view.frame.size.height/2 - errorSize.height/2, errorSize.width, errorSize.height)];
+    [errorLabel setTextColor:HEXCOLOR(0x151848)];
+    errorLabel.backgroundColor = [UIColor clearColor];
+    errorLabel.text = error;
+    errorLabel.textAlignment = UITextAlignmentCenter;    
+    [self.view addSubview:errorLabel];
+    [errorLabel release];
 }
 
 - (void)setupView {
-    if (!dropboxMessage) {
-        UIFont* errorFont = [UIFont fontWithName:@"Helvetica-Bold" size:19];
-        NSString* error = NSLocalizedString(@"Error loading dropbox message.",@"Statement that there is an error loading a dropbox item.");
-        CGSize maximumSize = CGSizeMake(320, 1000);
-        CGSize errorSize = [error sizeWithFont:errorFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap];
-        UILabel* errorLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.view.frame.size.width/2 - errorSize.width/2, self.view.frame.size.height/2 - errorSize.height/2, errorSize.width, errorSize.height)];
-        [errorLabel setTextColor:HEXCOLOR(0x151848)];
-        errorLabel.backgroundColor = [UIColor clearColor];
-        errorLabel.text = error;
-        errorLabel.textAlignment = UITextAlignmentCenter;
+    DropboxBasket* basket = (DropboxBasket*)self.dropboxBasket;
+    DropboxMessage* message = (DropboxMessage*)self.dropboxMessage;
+    
+    // set up some colors
+    UIColor *headerFontColor = HEXCOLOR(0x151848);
+    UIColor *subheaderFontColor = HEXCOLOR(0x005B92);
+    UIColor *normalTextColor = HEXCOLOR(0x262626);
+    UIColor *buttonTextColor = HEXCOLOR(0x5A5A5A);
+    
+    // set up some fonts
+    UIFont* courseNameFont = [UIFont fontWithName:@"Helvetica-Bold" size:13];
+    UIFont* titleFont = [UIFont fontWithName:@"Helvetica-Bold" size:19];
+    UIFont* subheaderFont = [UIFont fontWithName:@"Helvetica-Bold" size:14];
+    UIFont* commentsFont = [UIFont fontWithName:@"Helvetica" size:13];
+    UIFont* dateFont = [UIFont fontWithName:@"Helvetica-Oblique" size:12];
+    UIFont* buttonFont = [UIFont fontWithName:@"Helvetica-Bold" size:12];
+    
+    // set up the course name label
+    Course* course = [[eCollegeAppDelegate delegate] getCourseHavingId:courseId];
+    NSString *courseName = course.title;    
+    CGSize maximumSize = CGSizeMake(284.0, 1000.0);
+    CGSize courseNameSize = [courseName sizeWithFont:courseNameFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap]; // 18px left, right margins, so 284.0 width
+    UILabel* courseNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(18, 16, courseNameSize.width, courseNameSize.height)];
+    courseNameLabel.font = courseNameFont;
+    courseNameLabel.textColor = headerFontColor;
+    courseNameLabel.lineBreakMode = UILineBreakModeWordWrap;
+    courseNameLabel.text = courseName;
+    courseNameLabel.backgroundColor = [UIColor clearColor];
+    courseNameLabel.numberOfLines = 0;
+    [self.view addSubview:courseNameLabel];
+    
+    // set up the assignment title label
+    NSString* assignmentName = basket.title;
+    CGSize assignmentNameSize = [assignmentName sizeWithFont:titleFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap];
+    UILabel* assignmentLabel = [[UILabel alloc] initWithFrame:CGRectMake(18, courseNameLabel.frame.origin.y + courseNameLabel.frame.size.height + 5, assignmentNameSize.width, assignmentNameSize.height)];
+    assignmentLabel.font = titleFont;
+    assignmentLabel.textColor = headerFontColor;
+    assignmentLabel.lineBreakMode = UILineBreakModeWordWrap;
+    assignmentLabel.text = assignmentName;
+    assignmentLabel.backgroundColor =  [UIColor clearColor];
+    assignmentLabel.numberOfLines = 0;
+    [self.view addSubview:assignmentLabel];
+    
+    // set up the white box in the background, with rounded corners and drop shadow (arbitrary initial height, will change that later)
+    UIView* whiteBox = [[UIView alloc] initWithFrame:CGRectMake(9, assignmentLabel.frame.origin.y + assignmentLabel.frame.size.height + 10, 303, 500)];
+    whiteBox.backgroundColor = [UIColor whiteColor];
+    whiteBox.layer.cornerRadius = 10.0;
+    whiteBox.layer.shadowColor = [[UIColor lightGrayColor] CGColor];
+    whiteBox.layer.shadowRadius = 1.0;
+    whiteBox.layer.shadowOpacity = 0.8;
+    whiteBox.layer.shadowOffset = CGSizeMake(0, 2);
+    [self.view addSubview:whiteBox];
+    
+    // set up the image
+    UIImageView* img = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 25, 25)];
+    img.image = [UIImage imageNamed:@"clock.png"];
+    [whiteBox addSubview:img];    
+    
+    // set up the 'Posted By' label
+    NSString* postedByText = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"Posted by",nil), [message nameOfSubmissionStudent]];
+    CGSize postedBySize = [postedByText sizeWithFont:commentsFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap];
+    UILabel* postedByLabel = [[UILabel alloc] initWithFrame:CGRectMake(45, 16, postedBySize.width, postedBySize.height)];
+    postedByLabel.font = commentsFont;
+    postedByLabel.textColor = normalTextColor;
+    postedByLabel.text = postedByText;
+    postedByLabel.backgroundColor = [UIColor clearColor];
+    postedByLabel.numberOfLines = 0;
+    [whiteBox addSubview:postedByLabel];
+    
+    // set up the date label
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    [gregorian setTimeZone:[NSTimeZone defaultTimeZone]];
+    DateCalculator* dateCalculator = [[DateCalculator alloc] initWithCalendar:gregorian];
+    [gregorian release];
+    int numDays = [dateCalculator datesFrom:[NSDate date] to:message.date];
+    NSString* dateString = [message.date friendlyDateWithTimeFor:numDays];
+    CGSize dateSize = [dateString sizeWithFont:dateFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap];
+    UILabel* dateLabel = [[UILabel alloc] initWithFrame:CGRectMake(45, postedByLabel.frame.origin.y + postedByLabel.frame.size.height + 5, dateSize.width, dateSize.height)];
+    dateLabel.font = dateFont;
+    dateLabel.textColor = normalTextColor;
+    dateLabel.text = dateString;
+    dateLabel.backgroundColor = [UIColor clearColor];
+    dateLabel.numberOfLines = 0;
+    [whiteBox addSubview:dateLabel];
+    
+    // set up the comments label
+    NSString* comments = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"Comments",nil), message.comments];
+    maximumSize = CGSizeMake(243, 2000);
+    CGSize commentsSize = [comments sizeWithFont:commentsFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap];
+    UILabel* commentsLabel = [[UILabel alloc] initWithFrame:CGRectMake(45, dateLabel.frame.origin.y + dateLabel.frame.size.height + 5, commentsSize.width, commentsSize.height)];
+    commentsLabel.font = commentsFont;
+    commentsLabel.textColor = normalTextColor;
+    commentsLabel.text = comments;
+    commentsLabel.backgroundColor = [UIColor clearColor];
+    commentsLabel.numberOfLines = 0;
+    [whiteBox addSubview:commentsLabel];
+    
+    // set up a button for each attachment
+    int count = 0;
+    UIButton* btn = nil;
+    
+    while (count < [message.attachments count]) {
         
-        [self.view addSubview:errorLabel];
-        [errorLabel release];
-    } else {
+        // grab the attachment
+        DropboxAttachment *attachment = [message.attachments objectAtIndex:count];
         
-        DropboxBasket* basket = (DropboxBasket*)self.dropboxBasket;
-        DropboxMessage* message = (DropboxMessage*)self.dropboxMessage;
-        
-        // set up some colors
-        UIColor *headerFontColor = HEXCOLOR(0x151848);
-        UIColor *subheaderFontColor = HEXCOLOR(0x005B92);
-        UIColor *normalTextColor = HEXCOLOR(0x262626);
-        UIColor *buttonTextColor = HEXCOLOR(0x5A5A5A);
-        
-        // set up some fonts
-        UIFont* courseNameFont = [UIFont fontWithName:@"Helvetica-Bold" size:13];
-        UIFont* titleFont = [UIFont fontWithName:@"Helvetica-Bold" size:19];
-        UIFont* subheaderFont = [UIFont fontWithName:@"Helvetica-Bold" size:14];
-        UIFont* commentsFont = [UIFont fontWithName:@"Helvetica" size:13];
-        UIFont* dateFont = [UIFont fontWithName:@"Helvetica-Oblique" size:12];
-        UIFont* buttonFont = [UIFont fontWithName:@"Helvetica-Bold" size:12];
-        
-        // set up the course name label
-        Course* course = [[eCollegeAppDelegate delegate] getCourseHavingId:courseId];
-        NSString *courseName = course.title;    
-        CGSize maximumSize = CGSizeMake(284.0, 1000.0);
-        CGSize courseNameSize = [courseName sizeWithFont:courseNameFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap]; // 18px left, right margins, so 284.0 width
-        UILabel* courseNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(18, 16, courseNameSize.width, courseNameSize.height)];
-        courseNameLabel.font = courseNameFont;
-        courseNameLabel.textColor = headerFontColor;
-        courseNameLabel.lineBreakMode = UILineBreakModeWordWrap;
-        courseNameLabel.text = courseName;
-        courseNameLabel.backgroundColor = [UIColor clearColor];
-        courseNameLabel.numberOfLines = 0;
-        [self.view addSubview:courseNameLabel];
-        
-        // set up the assignment title label
-        NSString* assignmentName = basket.title;
-        CGSize assignmentNameSize = [assignmentName sizeWithFont:titleFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap];
-        UILabel* assignmentLabel = [[UILabel alloc] initWithFrame:CGRectMake(18, courseNameLabel.frame.origin.y + courseNameLabel.frame.size.height + 5, assignmentNameSize.width, assignmentNameSize.height)];
-        assignmentLabel.font = titleFont;
-        assignmentLabel.textColor = headerFontColor;
-        assignmentLabel.lineBreakMode = UILineBreakModeWordWrap;
-        assignmentLabel.text = assignmentName;
-        assignmentLabel.backgroundColor =  [UIColor clearColor];
-        assignmentLabel.numberOfLines = 0;
-        [self.view addSubview:assignmentLabel];
-        
-        // set up the white box in the background, with rounded corners and drop shadow (arbitrary initial height, will change that later)
-        UIView* whiteBox = [[UIView alloc] initWithFrame:CGRectMake(9, assignmentLabel.frame.origin.y + assignmentLabel.frame.size.height + 10, 303, 500)];
-        whiteBox.backgroundColor = [UIColor whiteColor];
-        whiteBox.layer.cornerRadius = 10.0;
-        whiteBox.layer.shadowColor = [[UIColor lightGrayColor] CGColor];
-        whiteBox.layer.shadowRadius = 1.0;
-        whiteBox.layer.shadowOpacity = 0.8;
-        whiteBox.layer.shadowOffset = CGSizeMake(0, 2);
-        [self.view addSubview:whiteBox];
-        
-        // set up the image
-        UIImageView* img = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 25, 25)];
-        img.image = [UIImage imageNamed:@"clock.png"];
-        [whiteBox addSubview:img];    
-        
-        // set up the 'Posted By' label
-        NSString* postedByText = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"Posted by",nil), [message nameOfSubmissionStudent]];
-        CGSize postedBySize = [postedByText sizeWithFont:commentsFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap];
-        UILabel* postedByLabel = [[UILabel alloc] initWithFrame:CGRectMake(45, 16, postedBySize.width, postedBySize.height)];
-        postedByLabel.font = commentsFont;
-        postedByLabel.textColor = normalTextColor;
-        postedByLabel.text = postedByText;
-        postedByLabel.backgroundColor = [UIColor clearColor];
-        postedByLabel.numberOfLines = 0;
-        [whiteBox addSubview:postedByLabel];
-        
-        // set up the date label
-        NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-        [gregorian setTimeZone:[NSTimeZone defaultTimeZone]];
-        DateCalculator* dateCalculator = [[DateCalculator alloc] initWithCalendar:gregorian];
-        [gregorian release];
-        int numDays = [dateCalculator datesFrom:[NSDate date] to:message.date];
-        NSString* dateString = [message.date friendlyDateWithTimeFor:numDays];
-        CGSize dateSize = [dateString sizeWithFont:dateFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap];
-        UILabel* dateLabel = [[UILabel alloc] initWithFrame:CGRectMake(45, postedByLabel.frame.origin.y + postedByLabel.frame.size.height + 5, dateSize.width, dateSize.height)];
-        dateLabel.font = dateFont;
-        dateLabel.textColor = normalTextColor;
-        dateLabel.text = dateString;
-        dateLabel.backgroundColor = [UIColor clearColor];
-        dateLabel.numberOfLines = 0;
-        [whiteBox addSubview:dateLabel];
-        
-        // set up the comments label
-        NSString* comments = [NSString stringWithFormat:@"%@: %@", NSLocalizedString(@"Comments",nil), message.comments];
-        maximumSize = CGSizeMake(243, 2000);
-        CGSize commentsSize = [comments sizeWithFont:commentsFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap];
-        UILabel* commentsLabel = [[UILabel alloc] initWithFrame:CGRectMake(45, dateLabel.frame.origin.y + dateLabel.frame.size.height + 5, commentsSize.width, commentsSize.height)];
-        commentsLabel.font = commentsFont;
-        commentsLabel.textColor = normalTextColor;
-        commentsLabel.text = comments;
-        commentsLabel.backgroundColor = [UIColor clearColor];
-        commentsLabel.numberOfLines = 0;
-        [whiteBox addSubview:commentsLabel];
-        
-        // set up the list of attachments
-        NSString* allFiles = [NSString stringWithFormat:@"%@:",NSLocalizedString(@"Files",nil)];
-        for (NSString* attachment in message.attachments) {
-            allFiles = [NSString stringWithFormat:@"%@\n%@", allFiles, attachment];
+        // figure out the y coordinate of the new button we're making
+        int y;
+        if (count == 0) {
+            y = commentsLabel.frame.origin.y + commentsLabel.frame.size.height + 20;
+        } else {
+            y = btn.frame.origin.y + btn.frame.size.height + 10;
         }
-        CGSize filesSize = [allFiles sizeWithFont:commentsFont constrainedToSize:maximumSize lineBreakMode:UILineBreakModeWordWrap];
-        UILabel* filesLabel = [[UILabel alloc] initWithFrame:CGRectMake(45, commentsLabel.frame.origin.y + commentsLabel.frame.size.height + 5, filesSize.width, filesSize.height)];
-        filesLabel.font = commentsFont;
-        filesLabel.textColor = normalTextColor;
-        filesLabel.text = allFiles;
-        filesLabel.backgroundColor = [UIColor clearColor];
-        filesLabel.numberOfLines = 0;
-        [whiteBox addSubview:filesLabel];
         
-        //    // set up the "view all grades for this course" button
-        //    UIButton* button = [[UIButton alloc] initWithFrame:CGRectMake(45, dateLabel.frame.origin.y + dateLabel.frame.size.height + 20, 242, 30)];
-        //    button.titleLabel.font = buttonFont;
-        //    [button setTitle:NSLocalizedString(@"View all grades for this course", @"View all grades for this course") forState:UIControlStateNormal];
-        //    [button setTitleColor:buttonTextColor forState:UIControlStateNormal];
-        //    button.backgroundColor = HEXCOLOR(0xE9E9E9);
-        //    button.layer.cornerRadius = 3.0;
-        //    button.layer.borderWidth = 1.0;
-        //    button.layer.borderColor = [HEXCOLOR(0xC0C0C0) CGColor];
-        //    [whiteBox addSubview:button];
-        //    
-        // set the height of the white box
-        CGRect boxFrame = whiteBox.frame;
-        boxFrame.size.height = filesLabel.frame.origin.y + filesLabel.frame.size.height + 16; 
-        whiteBox.frame = boxFrame;
+        // if this isn't the first iteration through the loop, release the previous button
+        if (btn) {
+            [btn release];
+        }
         
-        [img release];
-        [filesLabel release];
-        [commentsLabel release];
-        [dateLabel release];
-        [postedByLabel release];
-        [assignmentLabel release];
-        [courseNameLabel release];
-        [whiteBox release];
-        
+        // make the new button
+        btn = [[UIButton alloc] initWithFrame:CGRectMake(45, y, 242, 30)];
+        btn.titleLabel.font = buttonFont;
+        [btn setTitle:attachment.name forState:UIControlStateNormal];
+        [btn setTitleColor:buttonTextColor forState:UIControlStateNormal];
+        btn.backgroundColor = HEXCOLOR(0xE9E9E9);
+        btn.layer.cornerRadius = 3.0;
+        btn.layer.borderWidth = 1.0;
+        btn.layer.borderColor = [HEXCOLOR(0xC0C0C0) CGColor];
+        [btn setTag:count];
+        [whiteBox addSubview:btn];
+        count += 1;
     }
+    
+    // set the height of the white box
+    CGRect boxFrame = whiteBox.frame;
+    if (btn) {
+        boxFrame.size.height = btn.frame.origin.y + btn.frame.size.height + 16; 
+    } else {
+        boxFrame.size.height = commentsLabel.frame.origin.y + commentsLabel.frame.size.height + 16;
+    }
+    whiteBox.frame = boxFrame;
+    
+    // memory management
+    [btn release];
+    [img release];
+    [commentsLabel release];
+    [dateLabel release];
+    [postedByLabel release];
+    [assignmentLabel release];
+    [courseNameLabel release];
+    [whiteBox release];
 }
 
 - (void)loadView {
