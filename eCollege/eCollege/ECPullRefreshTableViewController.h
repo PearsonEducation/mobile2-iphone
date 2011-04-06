92ea8bfda3e0d1490c711f82116f444c1c610d63
Tony Hillerson <tony.hillerson@gmail.com>
2011-04-06 13:46:51 -0600

    Adding view controller superclasses to help consolidate some logic

---
+++ eCollege/eCollege/ECPullRefreshTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  ECPulldownToRefreshViewController.h
+//  eCollege
+//
+//  Created by Tony Hillerson on 4/6/11.
+//  Copyright 2011 EffectiveUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PullRefreshTableViewController.h"
+
+@interface ECPullRefreshTableViewController : PullRefreshTableViewController {
+    
+}
+
+@end
---
+++ eCollege/eCollege/ECPullRefreshTableViewController.m
@@ -0,0 +1,44 @@
+//
+//  ECPulldownToRefreshViewController.m
+//  eCollege
+//
+//  Created by Tony Hillerson on 4/6/11.
+//  Copyright 2011 EffectiveUI. All rights reserved.
+//
+
+#import "ECPullRefreshTableViewController.h"
+#import "InfoTableViewController.h"
+
+@implementation ECPullRefreshTableViewController
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	UIImage *navBarLogoImage = [UIImage imageNamed:@"image_logo_small"];
+	UIImageView *navBarLogoView = [[[UIImageView alloc] initWithImage:navBarLogoImage] autorelease];
+	self.navigationItem.titleView = navBarLogoView;
+	
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeInfoLight];
+    [btn addTarget:self action:@selector(infoButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:btn];
+    [btn release];
+}
+
+- (void)infoButtonTapped:(id)sender {
+    InfoTableViewController* infoTableViewController = [[InfoTableViewController alloc] initWithNibName:@"InfoTableViewController" bundle:nil];
+    infoTableViewController.cancelDelegate = self;
+    UINavigationController *infoNavController = [[UINavigationController alloc] initWithRootViewController:infoTableViewController];
+    [self presentModalViewController:infoNavController animated:YES];
+    [infoNavController release];
+    [infoTableViewController release];
+}
+
+- (void)cancelButtonClicked:(id)sender {
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+@end
