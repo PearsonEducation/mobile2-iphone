4bad8bd748951a35175a39502748db20d307f9c7
Tony Hillerson <tony.hillerson@gmail.com>
2011-04-07 16:17:05 -0600

    A utility to stripping HTML from a string

---
+++ eCollege/eCollege/NSString+stripHTML.h
@@ -0,0 +1,16 @@
+//
+//  NSString+stripHTML.h
+//  eCollege
+//
+//  Created by Tony Hillerson on 4/7/11.
+//  Copyright 2011 EffectiveUI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSString (stripHTMLAdditions)
+
+- (NSString *) stripHTML;
+
+@end
---
+++ eCollege/eCollege/NSString+stripHTML.m
@@ -0,0 +1,33 @@
+//
+//  NSString+stripHTML.m
+//  eCollege
+//
+//  Created by Tony Hillerson on 4/7/11.
+//  Copyright 2011 EffectiveUI. All rights reserved.
+//
+
+#import "NSString+stripHTML.h"
+
+
+@implementation NSString (stripHTMLAdditions)
+
+- (NSString *) stripHTML {
+	NSString *tagHolder = nil;
+	NSString *flattenedString = self;
+	
+	NSScanner *scanner = [NSScanner scannerWithString:flattenedString];
+	
+    while ([scanner isAtEnd] == NO) {
+        [scanner scanUpToString:@"<" intoString:NULL]; 
+        [scanner scanUpToString:@">" intoString:&tagHolder];
+		
+        flattenedString = [flattenedString stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"%@>", tagHolder]
+																	 withString:@" "];
+    }
+	
+	flattenedString = [flattenedString stringByReplacingOccurrencesOfString:@"  " withString:@" "];
+    
+    return flattenedString;
+}
+
+@end
