6916e7f8c8cf50db631bbd4f7d78d6c801e4886a
Tony Hillerson <tony.hillerson@gmail.com>
2011-04-14 13:42:54 -0600

    Fixing some leaks in the app delegate

--- eCollege/eCollege/eCollegeAppDelegate.m
+++ eCollege/eCollege/eCollegeAppDelegate.m
@@ -193,28 +193,28 @@ int coursesRefreshInterval = 43200; // 12 hours = 43200 seconds
 
 - (void)showTabBar {
     // instantiate the tab bar
-    self.tabBarController = [[UITabBarController alloc] init];
+    self.tabBarController = [[[UITabBarController alloc] init] autorelease];
     
     // create an array to hold the tabs
-    NSMutableArray* allTabs = [[NSMutableArray alloc] initWithCapacity:5];
+    NSMutableArray* allTabs = [NSMutableArray arrayWithCapacity:5];
 
     UIImage* img = nil;
 	ECClientConfiguration *clientConfiguration = [ECClientConfiguration currentConfiguration];
 
     // Create the view controllers for the tabs
-    self.homeViewController = [[HomeViewController alloc] initWithNibName:@"HomeViewController" bundle:nil];
+    self.homeViewController = [[[HomeViewController alloc] initWithNibName:@"HomeViewController" bundle:nil] autorelease];
     img = [UIImage imageNamed:@"home_icon.png"];
     [self.homeViewController.tabBarItem initWithTitle:NSLocalizedString(@"Home", @"Label on the tab bar for the 'home' section") image:img tag:0];
     UINavigationController* homeNav = [clientConfiguration primaryNavigationControllerWithRootViewController:self.homeViewController];
     [allTabs addObject:homeNav];
 
-    self.discussionsViewController = [[DiscussionsViewController alloc] initWithNibName:@"DiscussionsViewController" bundle:nil];
+    self.discussionsViewController = [[[DiscussionsViewController alloc] initWithNibName:@"DiscussionsViewController" bundle:nil] autorelease];
     img = [UIImage imageNamed:@"discussions_icon.png"];
     [self.discussionsViewController.tabBarItem initWithTitle:NSLocalizedString(@"Discussions", @"Label on the tab bar for the 'discussions' section") image:img tag:0];
     UINavigationController* discussionsNav = [clientConfiguration primaryNavigationControllerWithRootViewController:self.discussionsViewController];
     [allTabs addObject:discussionsNav];
 
-    self.coursesViewController = [[CoursesViewController alloc] initWithNibName:@"CoursesViewController" bundle:nil];
+    self.coursesViewController = [[[CoursesViewController alloc] initWithNibName:@"CoursesViewController" bundle:nil] autorelease];
     img = [UIImage imageNamed:@"courses_icon.png"];
     [self.coursesViewController.tabBarItem initWithTitle:NSLocalizedString(@"Courses", @"Label on the tab bar for the 'courses' section") image:img tag:0];
     UINavigationController* coursesNav = [clientConfiguration primaryNavigationControllerWithRootViewController:self.coursesViewController];
@@ -226,7 +226,7 @@ int coursesRefreshInterval = 43200; // 12 hours = 43200 seconds
 //    UINavigationController* peopleNav = [clientConfiguration newPrimaryNavigationControllerWithRootViewController:self.peopleViewController];
 //    [allTabs addObject:peopleNav];
 //
-    self.profileViewController = [[ProfileViewController alloc] initWithNibName:@"ProfileViewController" bundle:nil];
+    self.profileViewController = [[[ProfileViewController alloc] initWithNibName:@"ProfileViewController" bundle:nil] autorelease];
     img = [UIImage imageNamed:@"my_profile_icon.png"];
     [self.profileViewController.tabBarItem initWithTitle:NSLocalizedString(@"Profile", @"Label on the tab bar for the 'profile' section") image:img tag:0];
     UINavigationController* profileNav = [clientConfiguration primaryNavigationControllerWithRootViewController:self.profileViewController];
@@ -241,7 +241,7 @@ int coursesRefreshInterval = 43200; // 12 hours = 43200 seconds
 
 - (void)showGlobalLoader {
     if (!self.blockingActivityView) {
-        self.blockingActivityView = [[BlockingActivityView alloc] initWithWithView:[UIApplication sharedApplication].keyWindow];
+        self.blockingActivityView = [[[BlockingActivityView alloc] initWithWithView:[UIApplication sharedApplication].keyWindow] autorelease];
         UIColor* bgColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.25];
         self.blockingActivityView.backgroundColor = bgColor;
     }
@@ -263,7 +263,7 @@ int coursesRefreshInterval = 43200; // 12 hours = 43200 seconds
 - (void)refreshCourseList {
     // if courses are already being fetched, don't initiate another call.
     if (!self.courseFetcher) {
-        self.courseFetcher = [[CourseFetcher alloc] initWithDelegate:self responseSelector:@selector(coursesLoaded:)];
+        self.courseFetcher = [[[CourseFetcher alloc] initWithDelegate:self responseSelector:@selector(coursesLoaded:)] autorelease];
         [courseFetcher fetchMyCurrentCourses];
     }    
 }
